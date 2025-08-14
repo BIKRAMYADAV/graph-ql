481527c4a0060bf5d982496792ca80925afa376e
BIKRAMYADAV <bikram.deep.yadav.11@gmail.com>
2025-08-15 02:09:23 +0530

    feat: added resolver functions

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { typeDefs } from "./schema";
+import { typeDefs } from "./schema.js";
+
+import db from "./db.js";
+
+const resolvers = {
+    Query : {
+        games() {
+            return db.games;
+        },
+        reviews() {
+            return db.reviews;
+        },
+        authors(){
+            return db.authors;
+        }
+    }
+}
 
 const server = new ApolloServer({
     //typedefs
     typeDefs,
     //resolvers
+    resolvers
 })
 
 const {url} = await startStandaloneServer(server, {
