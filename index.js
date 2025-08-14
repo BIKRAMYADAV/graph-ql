2dff4b7f4201cee84990cfa2204e10c8969fb6cc
BIKRAMYADAV <bikram.deep.yadav.11@gmail.com>
2025-08-15 01:31:16 +0530

    initialized files and folder

---
+++ index.js
@@ -0,0 +1,13 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const server = new ApolloServer({
+    //typedefs
+    //resolvers
+})
+
+const {url} = await startStandaloneServer(server, {
+    listen : {port : 4000}
+})
+
+console.log(`server is listening on port 4000`);
