dab6a6e23dba7a001af357c1c266217eb8e607e4
BIKRAMYADAV <bikram.deep.yadav.11@gmail.com>
2025-08-19 19:48:22 +0530

    feat: added resolver function to query particular author and game as well

--- schema.js
+++ schema.js
@@ -18,7 +18,9 @@ type Query {
     reviews : [Review]
     review(id:ID!): Review
     games : [Game]
+    game(id:ID!) : Game 
     authors : [Author]
+    author(id:ID!) : Author
 }
 `
 //type query shows the entry point
